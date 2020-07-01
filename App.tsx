eadb2ac6d2eec092a79852531335654eb32e20e7
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2020-07-01 22:49:29 +0200

    Add documentation

--- App.tsx
+++ App.tsx
@@ -1,12 +1,64 @@
 import React from "react";
-import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import { StyleSheet, Text, View, TextInput, Linking } from "react-native";
+import SyntaxHighlighter from "react-native-syntax-highlighter";
+
 import { useInputFocus } from "./hooks";
+
+const HOOK_CODE = `
+type InputLookup = { [key: string]: TextInput };
+
+export const useInputFocus = () => {
+  const refs = useRef<InputLookup>({});
+  const [
+    currentFocus, 
+    setCurrentFocus
+  ] = useState<string>("");
+  const focus = useCallback(
+    (key: string) => {
+      if (refs.current[currentFocus]) {
+        refs.current[currentFocus].blur();
+      }
+      if (refs.current[key]) {
+        refs.current[key].focus();
+      }
+    },
+    [currentFocus]
+  );
+  const setRef = (name: string) 
+      => (input: TextInput) 
+      => {
+    refs.current[name] = input;
+  };
+  return { focus, setRef };
+}; 
+`;
+
+const USE_HOOK_CODE = `
+const { focus, setRef } = useInputFocus();
+return (
+  <TextInput
+    ref={setRef("name")}
+    {/* Blur needed for multiline keyboard */}
+    blurOnSubmit={false} 
+    onSubmitEditing={() => focus("address")}
+  />
+  <TextInput
+    ref={setRef("address")}
+    onSubmitEditing={() => focus("name")}
+  />
+)
+`;
 
 export default function App() {
   const { focus, setRef } = useInputFocus();
+  const onGithubLinkPress = () =>
+    Linking.openURL("http://github.com/tomfa/react-native-input-focus");
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>Regular text inputs</Text>
+      <Text style={styles.h1}>React Native input autofocus</Text>
+      <Text style={styles.paragraph}>
+        The inputs will focus each other on submit.{" "}
+      </Text>
       <TextInput
         placeholder="Name"
         style={styles.input}
@@ -19,6 +71,18 @@ export default function App() {
         ref={setRef("address")}
         onSubmitEditing={() => focus("name")}
       />
+
+      <Text style={styles.h2}>Usage</Text>
+      <SyntaxHighlighter language="tsx" highlighter={"prism" || "hljs"}>
+        {USE_HOOK_CODE}
+      </SyntaxHighlighter>
+      <Text style={styles.h2}>Custom hook</Text>
+      <SyntaxHighlighter language="tsx" highlighter={"prism" || "hljs"}>
+        {HOOK_CODE}
+      </SyntaxHighlighter>
+      <Text style={[styles.footer, styles.link]} onPress={onGithubLinkPress}>
+        github.com/tomfa/react-native-input-focus
+      </Text>
     </View>
   );
 }
@@ -29,14 +93,33 @@ const styles = StyleSheet.create({
     backgroundColor: "#bebec3",
     alignItems: "center",
     justifyContent: "center",
+    paddingBottom: 100,
   },
   input: {
     padding: 12,
     backgroundColor: "#fff",
     marginBottom: 8,
   },
-  header: {
+  h1: {
+    fontSize: 36,
+    margin: 20,
+  },
+  h2: {
     fontSize: 18,
-    marginBottom: 8,
+    margin: 8,
+    marginTop: 18,
+  },
+  paragraph: {
+    marginBottom: 18,
+  },
+  footer: {
+    position: "absolute",
+    bottom: 0,
+    padding: 8,
+    marginBottom: 60
+  },
+  link: {
+    color: "blue",
+    textDecorationLine: "underline",
   },
 });
